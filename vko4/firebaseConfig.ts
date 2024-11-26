248e509586590547e175e5000e3ffb0ddad042cb
idakkr <ida.kriikkula1@gmail.com>
2024-11-26 23:58:53 +0200

    Update firebaseConfig.ts

--- vko4/firebaseConfig.ts
+++ vko4/firebaseConfig.ts
@@ -1,1 +1,10 @@
-
+const firebaseConfig = {
+    apiKey: "AIzaSyCxINnslfRE_XVw0z5SCnb2yXn4b-PE03o",
+    authDomain: "pilvipalvelut-6cd6b.firebaseapp.com",
+    projectId: "pilvipalvelut-6cd6b",
+    storageBucket: "pilvipalvelut-6cd6b.firebasestorage.app",
+    messagingSenderId: "1031892986058",
+    appId: "1:1031892986058:web:7704501acab42e19b12c33"
+  };
+  
+  export default firebaseConfig;
